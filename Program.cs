34c19f376bfd682c8cc88266afb0ad9623e1c6a4
DeepAshish <Pashish@live.ca>
2020-06-25 11:47:20 -0600

    Add comparative operatores logical operator is equal to not equal to

--- Program.cs
+++ Program.cs
@@ -7,60 +7,67 @@ namespace c_sharp_intro
         static void Main(string[] args)
         {
             
-            //"=" is the assignment operator
-            //String datatype is used for text.
+            
+            
+            //logical operator 
+
+            //Or will evaluate to true if eigher side is true.
+            Console.WriteLine (true || true); // || is OR
+            Console.WriteLine (false || true); // True
+            Console.WriteLine (false || false); // True
+
+
+            // And  will evaluate to true only if boith sides are true 
+            Console.WriteLine (true && true); //trye. // && is and 
+            Console.WriteLine (false && true); // false.
+             Console.WriteLine (false && false); // false.
+            
+
+
+        // how does this behave with different datatype
+        //Console.WriteLine (false || true); it's doesnot allow
+
+        //comparitive operators
+        // "equal to check.
+        Console.WriteLine (false == false );// True
+        Console.WriteLine (true == false );// false
+        Console.WriteLine (2.25==2.25);// trrue
+        Console.WriteLine (2.25 == 3.25);// false
+        //Console.WriteLine (3.14 =="3.14"); //cannot check tow different types.
+
+
+
+        // greater than check
+
+        Console.WriteLine (6>7);//false
+
+        Console.WriteLine (10>5);
+
+
+        // less than check
+
+        Console.WriteLine (6<7);//true
+        Console.WriteLine (10<5);//false
+
+
+        //greater than or equal to check
+        Console.WriteLine (5>=5);//true
+        Console.WriteLine (10>=5);//true
+        Console.WriteLine (4>=5);//false
+
+
+        //less than or equal to check
+        Console.WriteLine (5<=5);//true
+        Console.WriteLine (10<=5);//false
+        Console.WriteLine (4<=5);//true
+
+
+        //Not equal to check
+        Console.WriteLine ( false != false ); //false
+        Console.WriteLine ( true != false ); //true
+        Console.WriteLine ( 5 != 5 ); //false
+        Console.WriteLine ( 5 != 10 ); //true
         
-            String mystring = "This is a string!";
-            int myint = 42;
-
-            Double myDouble = 33.666;
-
-            bool mybool = true;
-
-            //subtraction operator is "-" (minus)
-            Console.WriteLine (myint - 10); //32
-
-            //multicplication operaton is "*" 
-
-            Console.WriteLine (myint * 2);
-
-
-            //Divison operator 
-            Console.WriteLine(myint / 2); //21
-
-            //Modulus operator is "%"
-            Console.WriteLine(myint % 13);
-
-            //Addition operator is "+"
-
-            Console.WriteLine(myint + 8); //8
-
-            Console.WriteLine( "3" + "3");
-            Console.WriteLine( "3" + 3);
-            Console.WriteLine(3 + 6 + "3");
-
-
-
-            //output our variable
-            Console.WriteLine (myint); //42
-            // a new assignment on an existing variable
-            myint = myint + 2; // 44
-            Console.WriteLine(myint);
-            
-            //myint = myint +"2"; //We cant not turn myint into string!
-
-            myint++; // the same as typing  Myint = myint + 1;
-            Console.WriteLine(myint);
-            myint--; // the same as typing  Myint = myint - 1;
-            Console.WriteLine(myint); //44
-
-            myint += 3; // the same as typing; myint = myint + 3
-            myint -= 5; // The same as typing myint = myint - 5
-            Console .WriteLine (myint); //42
-            
-
-
-
 
 
 
