c7e58e22ecaa7ac044de6934cb7c22ef1911ba17
DeepAshish <Pashish@live.ca>
2020-06-25 13:18:33 -0600

    if stament

--- Program.cs
+++ Program.cs
@@ -7,71 +7,23 @@ namespace c_sharp_intro
         static void Main(string[] args)
         {
             
+            Console.WriteLine("please enter a string");
+            string userInput = Console.ReadLine(); 
+
+           if (userInput.Length > 0) 
+           {
+               Console.WriteLine ("The user entered a value");
+
+           }
+
+           else 
+
+           {
+
+               Console.WriteLine ("The user did not entered a value");
+
+           }
             
-            
-            //logical operator 
-
-            //Or will evaluate to true if eigher side is true.
-            Console.WriteLine (true || true); // || is OR
-            Console.WriteLine (false || true); // True
-            Console.WriteLine (false || false); // True
-
-
-            // And  will evaluate to true only if boith sides are true 
-            Console.WriteLine (true && true); //trye. // && is and 
-            Console.WriteLine (false && true); // false.
-             Console.WriteLine (false && false); // false.
-            
-
-
-        // how does this behave with different datatype
-        //Console.WriteLine (false || true); it's doesnot allow
-
-        //comparitive operators
-        // "equal to check.
-        Console.WriteLine (false == false );// True
-        Console.WriteLine (true == false );// false
-        Console.WriteLine (2.25==2.25);// trrue
-        Console.WriteLine (2.25 == 3.25);// false
-        //Console.WriteLine (3.14 =="3.14"); //cannot check tow different types.
-
-
-
-        // greater than check
-
-        Console.WriteLine (6>7);//false
-
-        Console.WriteLine (10>5);
-
-
-        // less than check
-
-        Console.WriteLine (6<7);//true
-        Console.WriteLine (10<5);//false
-
-
-        //greater than or equal to check
-        Console.WriteLine (5>=5);//true
-        Console.WriteLine (10>=5);//true
-        Console.WriteLine (4>=5);//false
-
-
-        //less than or equal to check
-        Console.WriteLine (5<=5);//true
-        Console.WriteLine (10<=5);//false
-        Console.WriteLine (4<=5);//true
-
-
-        //Not equal to check
-        Console.WriteLine ( false != false ); //false
-        Console.WriteLine ( true != false ); //true
-        Console.WriteLine ( 5 != 5 ); //false
-        Console.WriteLine ( 5 != 10 ); //true
-        
-
-
-
-
 
 
         }
