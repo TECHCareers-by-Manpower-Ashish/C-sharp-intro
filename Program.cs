bac2fb7e928b45d297940259490d65585562aa19
DeepAshish <Pashish@live.ca>
2020-06-25 14:23:22 -0600

    else if

--- Program.cs
+++ Program.cs
@@ -6,36 +6,41 @@ namespace c_sharp_intro
     {
         static void Main(string[] args)
         {
-           Console.WriteLine ("please tell us if you want to add or subtreact:(add/Subtract)");
-           string useroparator = Console.ReadLine();
-           if  (useroparator =="add")
-           Console.WriteLine ("please enter the first number:");
-           int firstNum = Int32.Parse(Console.ReadLine () );
-           Console.WriteLine("please enter the second number to add");
-           int secondNum = Int32.Parse(Console.ReadLine() );
+           
 
-
-            int result = firstNum + secondNum ;
-            Console.WriteLine ("{0} + {1} = {2}", firstNum, secondNum, result  );
-
-            else if (useroparator == "subtract")
-
+Console.WriteLine ("Enter a number");
+int userint = int.Parse(Console.ReadLine ());
+if (userint >5)
 
 {
 
-           
-   
-           Console.WriteLine ("please enter the first number:");
-           int firstNum = Int32.Parse(Console.ReadLine () );
-           Console.WriteLine("please enter the second number to subtract");
-           int secondNum = Int32.Parse(Console.ReadLine() );
+    Console.WriteLine("greater than 5");
 
+}
+else if (userint > 10 )
 
-            int result = firstNum - secondNum ;
-            Console.WriteLine ("{0} -  {1} = {2}", firstNum, secondNum, result  );
+{
+Console.WriteLine ("A negative number");
+
+}
+
+else if (userint <0)
+
+{
+Console.WriteLine ("A negative number");
+
+}
+
+else
+{Console.WriteLine("Number is between 0 and 5");
 
 
 }
+
+
+
+
+
 
 }
          
