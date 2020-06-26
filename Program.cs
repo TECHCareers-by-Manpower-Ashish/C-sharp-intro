78258c14f364d84f5683b74bf601f9ceab36cf37
DeepAshish <Pashish@live.ca>
2020-06-26 09:21:23 -0600

    bin

--- Program.cs
+++ Program.cs
@@ -7,55 +7,7 @@ namespace c_sharp_intro
         static void Main(string[] args)
         {
         
-        // Guide the user...
-            Console.WriteLine( "Please enter add/subtract/multiply/exit:" );
-
-            // Collect the user input...
-            string userInput;
-            while ( ( userInput = Console.ReadLine() ) != "exit" ) // LOOPS until a user enters "exit" command.
-            {
-                int firstNum;
-                int secondNum;
-                int result;
-                switch ( userInput )
-                {
-                    case "add":
-
-                    Console.WriteLine ("Enter first number for addition");
-                    int firstNum = Int32.Parse (Console.ReadLine ());
-                    Console.WriteLine ("Enter first number for addition");
-                     int secondNum = Int32.Parse (Console.ReadLine ());
-                    int result = firstNum + secondNum ;
-                    Console.WriteLine("{0} + {1} = {2}", firstNum , secondNum , result );
-
-                        break;
-                    case "subtract":
-                    Console.WriteLine ("Enter first number for subtract");
-                    int firstNum = Int32.Parse (Console.ReadLine ());
-                    Console.WriteLine ("Enter first number for subtract");
-                     int secondNum = Int32.Parse (Console.ReadLine ());
-                    int result = firstNum + secondNum ;
-                    Console.WriteLine("{0} + {1} = {2}", firstNum , secondNum , result );
-                      break;
-                    case "multiply":
-                    
-                    Console.WriteLine ("Enter first number for multiply");
-                    int firstNum = Int32.Parse (Console.ReadLine ());
-                    Console.WriteLine ("Enter first number for multiply");
-                     int secondNum = Int32.Parse (Console.ReadLine ());
-                    int result = firstNum + secondNum ;
-                    Console.WriteLine("{0} + {1} = {2}", firstNum , secondNum , result );
-
-                        break;
-                    default:
-
-                    Console.WriteLine ("unrecognize character");
-
-                        break;
-                }
-            }
-
-
+        
 
 
 
