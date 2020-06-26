c4868ba4f7428e911ba17901701af70a38a75bfb
DeepAshish <Pashish@live.ca>
2020-06-26 11:48:19 -0600

    exception messages

--- Program.cs
+++ Program.cs
@@ -6,29 +6,105 @@ namespace c_sharp_intro
     {
         static void Main(string[] args)
         {
-        
+            Console.WriteLine("please type: (add/substract/exit)");
+            string command;
+            while((command = Console.ReadLine() ) != "exit")
 
-        // 2 dimenson array!
-        
-string[,] topics = {{"cats", "dog", "dog", "hedgehog" },
+{
 
-                    {"zebra","gifaffe","lion", "elephant"},
-                     {"dolphine", "octopus", "shark", "whale"},
-                     {"bug","beetle","waterbug", "mosquito"}};     
 
-            for( int row = 0; row < topics.GetLength(0); row++ )
-            {
-                // Output current row...
-                Console.WriteLine( "Starting row #{0}", row );
-                // Loop through columns in the current row...
-                for ( int column = 0; column < topics.GetLength(1); column++ )
-                {
-                    // Output the current column...
-                    Console.WriteLine( "Starting column #{0}", column );
-                    // Output the current animal!
-                    Console.WriteLine( "This animal is: {0}", topics[row,column] );
-                }
-            }
-        }
-    }
+int result;
+if ( command == "add")
+{
+Console.WriteLine ("Enter first number to add:");
+int firstNum = int.TryParse (Console.ReadLine());
+Console.WriteLine ("Enter second number to add:");
+int secondNum = int.TryParse (Console.ReadLine());
+result = Addition(firstNum, secondNum);
+
 }
+
+else if ( command == "subtract")
+{
+Console.WriteLine ("Enter first number to add:");
+int firstNum = int.TryParse (Console.ReadLine());
+Console.WriteLine ("Enter second number to add:");
+int secondNum = int.TryParse (Console.ReadLine());
+result = Addition(firstNum, secondNum);
+
+}
+else 
+{
+Console.WriteLine("Invalid command");
+
+
+
+}
+
+
+
+
+{
+
+static int Addition( int num1, int num2 );
+
+}
+
+
+
+{
+
+return num1 + num2;
+
+}
+
+static int subtraction( int num1, int num2);
+
+{
+
+return num1 - num2;
+
+}
+
+        
+static int collectIntfromuser()
+
+{
+
+
+bool error =  true;
+while (error == true)
+
+{
+
+string uservalue = Console.ReadLine();
+try
+
+{
+
+int intvalue = int.Parse(uservalue);
+return intvalue;
+error = false;
+
+}
+
+
+catch ( Exception exception ) {}
+
+
+Console.WriteLine("invalid value entered. Please enter a number");
+
+Console.WriteLine( exception.Message);
+
+
+}
+
+
+}
+
+return intValue;
+
+            } }
+        
+    
+    
