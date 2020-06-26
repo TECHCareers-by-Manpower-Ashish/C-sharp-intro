c0b1875bcb5ed0c49d7f75c00981f471d0df6e73
DeepAshish <Pashish@live.ca>
2020-06-26 10:26:51 -0600

    add for loop example with 2- d array

--- Program.cs
+++ Program.cs
@@ -7,14 +7,28 @@ namespace c_sharp_intro
         static void Main(string[] args)
         {
         
+
+        // 2 dimenson array!
         
+string[,] topics = {{"cats", "dog", "dog", "hedgehog" },
 
+                    {"zebra","gifaffe","lion", "elephant"},
+                     {"dolphine", "octopus", "shark", "whale"},
+                     {"bug","beetle","waterbug", "mosquito"}};     
 
-
-
-}
-         
+            for( int row = 0; row < topics.GetLength(0); row++ )
+            {
+                // Output current row...
+                Console.WriteLine( "Starting row #{0}", row );
+                // Loop through columns in the current row...
+                for ( int column = 0; column < topics.GetLength(1); column++ )
+                {
+                    // Output the current column...
+                    Console.WriteLine( "Starting column #{0}", column );
+                    // Output the current animal!
+                    Console.WriteLine( "This animal is: {0}", topics[row,column] );
+                }
+            }
         }
     }
-
-
+}
